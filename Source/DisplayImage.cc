b57f06b38e4e466b4ba818355b9ecd966d2c2432
T.J. Corona <tjcorona@physics.unc.edu>
2015-01-20 11:16:56 -0500

    Messing with spacing.

--- Source/DisplayImage.cc
+++ Source/DisplayImage.cc
@@ -1,24 +1,24 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-int main(int argc, char** argv )
+int main(int argc,char** argv)
 {
-  if ( argc != 2 )
+  if (argc != 2)
   {
     std::cout<<"usage: DisplayImage <Image_Path>"<<std::endl;
     return -1;
   }
 
   cv::Mat image;
-  image = cv::imread( argv[1], 1 );
+  image = cv::imread(argv[1],1);
 
-  if ( !image.data )
+  if (!image.data)
   {
     std::cout<<"No image data"<<std::endl;
     return -1;
   }
-  cv::namedWindow("Display Image", CV_WINDOW_AUTOSIZE );
-  cv::imshow("Display Image", image);
+  cv::namedWindow("Display Image",CV_WINDOW_AUTOSIZE);
+  cv::imshow("Display Image",image);
 
   cv::waitKey(0);
 
